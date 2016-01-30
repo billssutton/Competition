d4635912b855f30fb100c2a9e377d0bb3cd3d028
billssutton <wsutto@gmail.com>
2016-01-29 22:21:19 -0500

    Initial Commit
    Initial commit for competition

---
+++ src/main/web/services/NameServer.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright 2015 Defense Health Agency (DHA)
+ *
+ * If your use of this software does not include any GPLv2 components:
+ * 	Licensed under the Apache License, Version 2.0 (the "License");
+ * 	you may not use this file except in compliance with the License.
+ * 	You may obtain a copy of the License at
+ *
+ * 	  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * 	Unless required by applicable law or agreed to in writing, software
+ * 	distributed under the License is distributed on an "AS IS" BASIS,
+ * 	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * 	See the License for the specific language governing permissions and
+ * 	limitations under the License.
+ * ----------------------------------------------------------------------------
+ * If your use of this software includes any GPLv2 components:
+ * 	This program is free software; you can redistribute it and/or
+ * 	modify it under the terms of the GNU General Public License
+ * 	as published by the Free Software Foundation; either version 2
+ * 	of the License, or (at your option) any later version.
+ *
+ * 	This program is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * 	GNU General Public License for more details.
+ *******************************************************************************/
+package main.web.services;
+
+import java.util.Hashtable;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+
+import org.apache.log4j.Logger;
+
+@Path("/engine")
+public class NameServer {
+
+	@Context
+	ServletContext context;
+	Logger logger = Logger.getLogger(NameServer.class.getName());
+	String output = "";
+	Hashtable helpHash = null;
+
+	// gets the engine resource necessary for all engine calls
+	@GET
+	@Path("test")
+	public Object getLocalDatabase(@Context HttpServletRequest request){
+		return "hey yo";
+	}
+
+}
